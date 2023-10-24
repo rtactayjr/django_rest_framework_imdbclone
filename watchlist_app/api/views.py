74d7aae226e929ff4f64401d9a8587e44db2a470
r-tactay <rolandtactay571@gmail.com>
2023-10-24 23:53:46 +0800

    add status codes to functions inside views.py

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-
+from rest_framewor import status
 from ..models import Movie
 from .serializers import MovieSerializer
 
@@ -26,8 +26,14 @@ def movie_list(request):
     
 @api_view(['GET', 'PUT','DELETE'])
 def movie_details(request, pk):
+    
     if request.method == 'GET':
-        movie = Movie.objects.get(pk=pk)
+        
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'Error': 'Movie not Found'}, status=status.HTTP_404_NOT_FOUND)
+        
         serializer = MovieSerializer(movie)
         return Response(serializer.data)
     
@@ -36,11 +42,11 @@ def movie_details(request, pk):
         serializer = MovieSerializer(movie, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         else:
             serializer.errors
         
     if request.method == 'DELETE':
         movie = Movie.objects.get(pk=pk)
         movie.delete() 
-        return Response()
+        return Response(status=status.HTTP_204_NO_CONTENT)
